87f34338368495b833ea6c3d77813095cd142526
Kshitij079-cmd <rajvanshikshitij@gmail.com>
2024-08-24 18:18:14 +0530

    24_aug

--- admin-dashboard/dashboardBackend/index.mjs
+++ admin-dashboard/dashboardBackend/index.mjs
@@ -1,11 +1,36 @@
+import {MongoClient, ServerApiVersion} from 'mongodb'
 import mongoose from "mongoose";
 import express from 'express';
 import cors from 'cors';
 const port = 5001;
-
-const ThreadsURI= "mongodb://127.0.0.1:27017/Threads"
-const  Users_URI = "mongodb://127.0.0.1:27017/Users"
+const ChattingURI= "mongodb+srv://rajvanshikshitij:Hitman@007@clusterchat.s0fr0.mongodb.net/?retryWrites=true&w=majority&appName=ClusterCHAT/"
+const ThreadsURI= "/Threads"
+const  Users_URI = "mongodb://127.0.0.1:27017/Users";
+const client = new MongoClient(ChattingURI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 const multiConnection =()=>{
+    // const ChattingDB=  mongoose.createConnection(ChattingURI, {
+    //     useUnifiedTopology: true,
+    //     useNewUrlParser: true
+    // })
     const ThreadsDB=  mongoose.createConnection(ThreadsURI, {
         useUnifiedTopology: true,
         useNewUrlParser: true
@@ -20,7 +45,7 @@ const multiConnection =()=>{
 let app = express()
 app.use(cors());
 app.use(express.json());
-
+console.log(multiConnection()," is connected")
 // axios.use()
 app.listen(port,()=>{
     console.log(`Dashboard backend is running on http://localhost:${port}`)})
